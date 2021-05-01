9a322f14d1ee4879f8bc0e941b80193302a3d9e2
Diego Echeverría <dhecheverria@uc.cl>
2021-05-01 18:22:32 -0400

    Added redraw function for graph

--- Scripts/draw.js
+++ Scripts/draw.js
@@ -1,49 +1,12 @@
-// function loadGraph(file) {
-//   console.log(file)
-//   anychart.data.loadJsonFile(file, function (data) {
-
-//     // create a chart from the loaded data
-//     var chart = anychart.graph(data);
-
-//     // set the title
-//     chart.title("Grafo entregado");
-
-//     // access nodes
-//     var nodes = chart.nodes();
-
-//     // set the size of nodes
-//     nodes.normal().height(30);
-//     nodes.hovered().height(45);
-//     nodes.selected().height(45);
-
-//     // set the stroke of nodes
-//     nodes.normal().stroke(null);
-//     nodes.hovered().stroke("#333333", 3);
-//     nodes.selected().stroke("#333333", 3);
-
-//     // enable the labels of nodes
-//     chart.nodes().labels().enabled(true);
-
-//     // configure the labels of nodes
-//     chart.nodes().labels().format("{%id}");
-//     chart.nodes().labels().fontSize(12);
-//     chart.nodes().labels().fontWeight(600);
-
-//     // draw the chart
-//     chart.container("graph-div").draw();
-
-//   });
-// };
-
-function loadGraph(data) {
+function createGraph(data) {
   // create a chart from the loaded data
-  var chart = anychart.graph(data);
+  const chart = anychart.graph();
 
   // set the title
   chart.title("Grafo entregado");
 
   // access nodes
-  var nodes = chart.nodes();
+  const nodes = chart.nodes();
 
   // set the size of nodes
   nodes.normal().height(30);
@@ -73,7 +36,52 @@ function loadGraph(data) {
   chart.edges().labels().fontSize(12);
   chart.edges().labels().fontWeight(600);
   chart.edges().tooltip().format("from: {%from}\nto: {%to}\nweight: {%weight}")
-
+  return chart;
 }
 
-export {loadGraph};
+function reDraw(chart, json) {
+  const newChart = anychart.graph();
+
+  // set the title
+  newChart.title("Grafo entregado");
+  newChart.data(json);
+
+  chart.container("graph-div").remove();
+  const parent = document.getElementById("graph-div")
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+  newChart.container("graph-div").draw()
+
+  const nodes = newChart.nodes();
+  const edges = newChart.edges();
+
+  // set the size of nodes
+  nodes.normal().height(30);
+  nodes.hovered().height(45);
+  nodes.selected().height(45);
+
+  // set the stroke of nodes
+  nodes.normal().stroke(null);
+  nodes.hovered().stroke("#333333", 3);
+  nodes.selected().stroke("#333333", 3);
+
+  // enable the labels of nodes
+  newChart.nodes().labels().enabled(true);
+
+  // configure the labels of nodes
+  newChart.nodes().labels().format("{%id}");
+  newChart.nodes().labels().fontSize(12);
+  newChart.nodes().labels().fontWeight(600);
+
+  newChart.edges().labels().enabled(true)
+
+
+  newChart.edges().labels().format("{%weight}")
+  newChart.edges().labels().fontSize(12);
+  newChart.edges().labels().fontWeight(600);
+  newChart.edges().tooltip().format("from: {%from}\nto: {%to}\nweight: {%weight}")
+  return newChart
+}
+
+export {createGraph, reDraw};
