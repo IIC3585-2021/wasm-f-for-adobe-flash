c4ced6fa2b6a06241aa2447f02a998fda487f9c3
Diego Echeverría <dhecheverria@uc.cl>
2021-05-01 21:00:11 -0400

    Added table results insertion on calculate

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,6 +5,8 @@ let chart = createGraph()
 let nodesSet = new Set();
 let edges = [];
 let jsonData = {nodes: [], edges: []};
+let temporalInput = "";
+let jsons = []
 
 function getNodesAndEdges(input) {
     if (!input) {
@@ -30,9 +32,9 @@ document.getElementById("calculate").addEventListener("click", calculateOptimal)
 
 function getInput() {
     const inputElement = document.getElementById("graph")
-    let input = inputElement.value;
+    temporalInput = inputElement.value;
     inputElement.value = "";
-    getNodesAndEdges(input);
+    getNodesAndEdges(temporalInput);
     let nodes = [];
     nodesSet.forEach(element => {
         nodes.push({"id": element});
@@ -50,6 +52,8 @@ function cleanGraph() {
 
 function calculateOptimal() {
     const optimalSolution = "abc";
+    const javascripTime = 2;
+    const cTime = 1;
     const solutionElements = optimalSolution.split("")
     let solutionEdges = [];
     for (let i = 0; i < (solutionElements.length - 1); i++) {
@@ -60,5 +64,26 @@ function calculateOptimal() {
             jsonData.edges[i] = {...element, stroke: {color: "#FFB27A", thickness: "3"}}
         }
     })
+    jsons.push(jsonData)
     reDraw(chart, jsonData)
+
+    const tableBody = document.getElementById("table-entries")
+    const tableRow = document.createElement("tr")
+    // tableRow.id = "table-row-" + jsons.length.
+
+    const firstCol = document.createElement("td")
+    firstCol.scope = "row"
+    firstCol.appendChild(document.createTextNode(jsons.length))
+
+    const secondCol = document.createElement("td")
+    secondCol.appendChild(document.createTextNode(javascripTime))
+
+    const thirdCol = document.createElement("td")
+    thirdCol.appendChild(document.createTextNode(cTime))
+
+    tableRow.appendChild(firstCol)
+    tableRow.appendChild(secondCol)
+    tableRow.appendChild(thirdCol)
+
+    tableBody.appendChild(tableRow)
 }
